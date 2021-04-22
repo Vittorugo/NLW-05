1e27134d78bcb6d417dc1e92ccdf0586755b1974
victtor hugo <hgloureirocosta@gmail.com>
2021-04-22 10:33:35 -0300

    Implementando funcionalidade de Play e Pause nos audios

--- podcastr-web/src/pages/episodes/[slug].tsx
+++ podcastr-web/src/pages/episodes/[slug].tsx
@@ -6,6 +6,7 @@ import { convertDurationToTimeString } from '../../utils/convertDurationToTimeSt
 import styles from './episode.module.scss';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 type Episode = {
    id: string;
@@ -24,6 +25,12 @@ type EpisodeProps = {
 }
 
 export default function Episode({ episode }: EpisodeProps) {
+
+   //const router = useRouter();
+
+   //if ( router.isFallback ) {
+   //   return <p> Carregando ...</p>
+   //}
 
    return (
       <div className={styles.episode}>
@@ -52,9 +59,26 @@ export default function Episode({ episode }: EpisodeProps) {
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
+
+   const { data } = await api.get('episodes', {
+      params: {
+        _limit: 2,
+        _sort: 'published_at',
+        _order: 'desc'
+      }
+    });
+
+    const paths = data.map(episode => {
+       return {
+          params: {
+             slug: episode.id
+          }
+       }
+    })
+
    return {
-      paths: [],
-      fallback: 'blocking'
+      paths,
+      fallback: 'blocking' // pode ser true - incremental fallback generation
    }
 }
 
